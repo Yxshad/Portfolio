3a8bff4b3f41b4b27f7d72a00e770b4682c9fe17
Yxshad <elavergne001@iutbayonne.univ-pau.fr>
2025-02-04 16:58:25 +0100

    feat !: ajout carrousel

--- fonctions.js
+++ fonctions.js
@@ -5,3 +5,18 @@
  * @Auteur : Elsa LAVERGNE (Shaggy)
  */
 
+function initCarrousel(){
+    const swiperVideo = new Swiper('.swiper', {
+        speed: 400,
+        spaceBetween: 100,
+        slidesPerView: 1,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    initCarrousel();
+});
